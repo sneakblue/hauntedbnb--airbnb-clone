6ebbf13da25a85b3218959c0fdaf3e08e0c1889d
david <sneakblue@gmail.com>
2021-10-06 10:07:54 -0600

    started building the booking component

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,6 +7,17 @@ const router = express.Router();
 
 router.get('/', asyncHandler( async (req, res) => {
     return res.json({ message: 'Booking api route reached'})
-}))
+}));
+
+router.post('/', asyncHandler( async (req, res) => {
+    const { hauntId, userId, startDate, endDate } = req.body;
+    const booking = await Booking.create({
+        hauntId,
+        userId,
+        startDate,
+        endDate
+    });
+    return res.json({ booking });
+}));
 
 module.exports = router;
---
+++ frontend/src/components/Bookings/index.js
@@ -0,0 +1,9 @@
+import './Bookings.css';
+
+export default function Bookings () {
+    return (
+        <div className='bookings-main--div'>
+            <h2>Booking Section</h2>
+        </div>
+    )
+}
