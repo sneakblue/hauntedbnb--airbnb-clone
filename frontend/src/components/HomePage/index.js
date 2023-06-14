c7b7b00d5436684f3d440f5249149421dce92b18
sneakblue <sneakblue@gmail.com>
2023-06-14 17:43:37 -0500

    added loading for homescreen

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getHaunts } from '../../store/haunts';
 import { useDispatch, useSelector } from 'react-redux';
+import ReactLoading from 'react-loading';
 import { getReviews } from '../../store/reviews';
 import HauntTile from '../HauntTile';
 import './HomePage.css';
@@ -10,43 +11,55 @@ export default function HomePage () {
     const [ loading, setLoading ] = useState(true);
     const haunts = useSelector(state => Object.values(state.haunts));
     useEffect(() => {
+        dispatch(getHaunts());
+        dispatch(getReviews());
 
         setTimeout(() => {
-            dispatch(getHaunts());
-            dispatch(getReviews());
             setLoading(false);
-        }, 2000)
+        }, 1000)
     }, [dispatch])
 
     let content;
-    if (loading) {
+    if (!loading) {
         content = (
             <>
                 <div className='title-div'>
-                <h1>Current Haunts</h1>
-            </div>
-            <div className='hauntTile-container'>
-                {haunts.map((haunt) => {
-                    return <HauntTile haunt={haunt} key={haunt.id}/>
-                })}
-            </div>
+                    <h1>Current Haunts</h1>
+                </div>
+                <div className='hauntTile-container'>
+                    {haunts.map((haunt) => {
+                        return <HauntTile haunt={haunt} key={haunt.id}/>
+                    })}
+                </div>
             </>
         )
     } else {
-        
+        content = (
+            <>
+                <div className='loading-div'>
+                    <ReactLoading
+                        type={"spinningBubbles"}
+                        color={'#F7F9F9'}
+                        height={150}
+                        width={150}
+                    />
+                </div>
+
+            </>
+        )
     }
 
     return (
         <div className='main-home-div'>
-
-            <div className='title-div'>
+            {/* <div className='title-div'>
                 <h1>Current Haunts</h1>
             </div>
             <div className='hauntTile-container'>
                 {haunts.map((haunt) => {
                     return <HauntTile haunt={haunt} key={haunt.id}/>
                 })}
-            </div>
+            </div> */}
+            {content}
         </div>
     )
 }
