17112ac15d72de87674b2fe6888b5b686d469e4b
sneakblue <sneakblue@gmail.com>
2023-06-14 17:07:20 -0500

    added react-loading package

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,23 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getHaunts } from '../../store/haunts';
 import { useDispatch, useSelector } from 'react-redux';
 import { getReviews } from '../../store/reviews';
-// import * as sessionActions from '../../store/session';
-// import { getAllHaunts } from '../../store/haunts';
 import HauntTile from '../HauntTile';
 import './HomePage.css';
 
 export default function HomePage () {
-    // const currUser = useSelector(state => state.session.user);
     const dispatch = useDispatch();
+    const [ loading, setLoading ] = useState(true);
     const haunts = useSelector(state => Object.values(state.haunts));
     useEffect(() => {
-        dispatch(getHaunts());
-        dispatch(getReviews());
+
+        setTimeout(() => {
+            dispatch(getHaunts());
+            dispatch(getReviews());
+            setLoading(false);
+        }, 2000)
     }, [dispatch])
+
+    let content;
+    if (loading) {
+        content = (
+            <>
+                <div className='title-div'>
+                <h1>Current Haunts</h1>
+            </div>
+            <div className='hauntTile-container'>
+                {haunts.map((haunt) => {
+                    return <HauntTile haunt={haunt} key={haunt.id}/>
+                })}
+            </div>
+            </>
+        )
+    } else {
+        
+    }
 
     return (
         <div className='main-home-div'>
+
             <div className='title-div'>
                 <h1>Current Haunts</h1>
             </div>
